64a925b4a74132f7fa3f921431dcb889ed420072
voidmain <lookupheaven@gmail.com>
2023-01-31 21:46:20 +0900

    init 2023.01.31

---
+++ src/main/java/com/fc10/passbatch/Fc10PassBatchApplication.java
@@ -0,0 +1,13 @@
+package com.fc10.passbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fc10PassBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fc10PassBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fc10/passbatch/Fc10PassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc10.passbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fc10PassBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
